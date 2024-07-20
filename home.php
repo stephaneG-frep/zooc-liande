e21a65ed1f6a61dcccb11fa352d2e6dda7ce989a
stephaneG-frep <119919153+stephaneG-frep@users.noreply.github.com>
2024-07-20 12:24:16 +0200

    suite

--- home.php
+++ home.php
@@ -33,7 +33,7 @@
             <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg"
                 style="background-image: url('uploads/images/maison.jpg');">
                 <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-                    <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Pour bien dormir auour du zoo
+                    <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Pour bien dormir autour du zoo
                     </h2>
                     <ul class="d-flex list-unstyled mt-auto">
                         <li class="me-auto">
